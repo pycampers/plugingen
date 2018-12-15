656f0bc0013f893adb7f283476d647f3acbf910b
devxpy <devxpy@gmail.com>
2018-12-15 18:51:42 +0530

    first commit

---
+++ plugingen.py
@@ -0,0 +1,75 @@
+import importlib
+import json
+import os
+import sys
+from textwrap import dedent
+from typing import Set, List
+
+__version__ = "0.0.1"
+
+CODE_TEMPLATE = dedent(
+    """
+    import sys
+    sys.path += {module_dirs}
+    sys.argv[1:] = {args}
+    {code}
+    """
+)
+
+
+def get_module_dir(module) -> str:
+    """Returns the proper package/module directory for `module`"""
+    try:
+        return os.path.dirname(module.__path__[0])
+    except (IndexError, AttributeError):
+        return os.path.dirname(module.__file__)
+
+
+def get_module_dirs(requirements: List[str]) -> Set[str]:
+    """Returns the proper package/module directory for all `requirements`"""
+    return {get_module_dir(i) for i in map(importlib.import_module, requirements)}
+
+
+def crash(plugin_name: str):
+    exit(
+        dedent(
+            """\
+            \033[91m
+            Oh no! This is a plugin cli which takes exactly 1 argument, as valid JSON.
+            It is only meant to be invoked programatically by \033[96m$ pip\033[91m.
+            \033[92m
+            You should probably run:
+
+                \033[96m $ pip {0} {1} \033[92m
+            
+            Instead of:
+            
+                \033[33m $ pip-plugin-{0} {1} \033[92m                      
+            """.format(
+                plugin_name, " ".join(sys.argv[1:])
+            )
+        )
+    )
+
+
+def generate_pip_plugin(
+    plugin_code: str, requirements: List[str] = [], plugin_name: str = "<plugin-name>"
+):
+    def plugin_cli():
+        if len(sys.argv) != 2:
+            crash(plugin_name)
+        try:
+            cmd = json.loads(sys.argv[1])
+        except json.JSONDecodeError:
+            crash(plugin_name)
+
+        code = CODE_TEMPLATE.format(
+            args=cmd["args"],
+            module_dirs=get_module_dirs(requirements),
+            code=plugin_code,
+        )
+
+        os.environ = cmd["os.environ"]
+        os.execv(cmd["sys.executable"], [cmd["sys.executable"], "-c", code])
+
+    return plugin_cli
